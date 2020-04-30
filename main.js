53f27220561f266695f062329bc4ebcf5740b1b1
Aaron Evan-Browning <aaevan@gmail.com>
2020-04-30 17:53:25 -0400

    Add initial round of projects.

--- main.js
+++ main.js
@@ -1,45 +1,81 @@
-//the problem: loop through an array and populate the styles of each project from a list of values:
-
-console.log("running main.js! (line 3)")
-
 const projects = [
   {
     name: "async game",
+    id: "asyncGame",
     description:
-      "a real-time text based game in python using asyncio and blessed"
+      "a real-time text based game in python using asyncio and blessed",
+    image: "images/async_game.png",
+    hoverImage: "images/async_game.gif",
+    site: "https://github.com/aaevan/asyncio_text_game"
   },
   {
     name: "caveExplore",
+    id: "caveExplore",
     description:
-      "Mostly an excuse to play with\
-       react-three-fiber and learn react-hooks"
+      "early prototype of a 3D exploration game in javascript (react)\
+       mostly an excuse to learn react-three-fiber and learn react-hooks",
+    image: "images/caveExplore.png",
+    hoverImage: "images/caveExplore.gif",
+    site: "https://github.com/aaevan/caveExplore"
   },
   {
     name: "pomodoro.sh",
-    description: "leverages osd_cat to write directly to the screen. a tool I made for myself and use daily."
+    id: "pomdoro",
+    description: "leverages osd_cat to write directly to the screen. a tool I made for myself and use daily.",
+    image: "images/pomodoro.png",
+    hoverImage: "images/pomodoro.gif",
+    site: "https://github.com/aaevan/aaevan_rc_files/blob/master/scripts/pomodoro.sh"
   },
   {
-    name: "flashy_bullshit.sh",
-    description: "comical misuse of osd_cat"
+    name: "pantry-monitor",
+    id: "pantryTracker",
+    description: "large satisfying buttons, notifications and a usb scale (featuring horrible manual byte manipulation)",
+    image: "images/pantry_monitor_1.png",
+    hoverImage: "images/pantry_monitor_2.png",
+    site: "https://github.com/aaevan/pantry-monitor"
+  },
+  {
+    name: "flashy_bullshit.sh\n(flashing lights)",
+    id: "flashy",
+    description: "comical misuse of osd_cat",
+    image: "images/flashy_bullshit.png",
+    hoverImage: "images/flashy_bullshit.gif",
+    site: "https://github.com/aaevan/aaevan_rc_files/blob/master/scripts/flashy_bullshit"
+  },
+  {
+    name: "Survival Series hotkey (Jenny Holzer)",
+    id: "holzer",
+    description: "xbindkeys + shell script \n(why? because she's great)",
+    image: "images/holzer_hotkey.png",
+    hoverImage: "images/holzer_hotkey.gif",
+    site: "https://github.com/aaevan/aaevan_rc_files/blob/master/scripts/randquote.sh"
   },
 ]
 
-//project-container
 const main = () => {
   const projectContainer = document.querySelector(".project-container");
   for (let project of projects) {
-    //console.log(project)
     let tempProject = document.createElement("div");
+    let styling = {
+      color: "red"
+    }
     tempProject.innerHTML =
       `
-        <div class="project">
-          <h2>${project.name}</h2>
+        <div class="project" id="${project.id}" href="${project.site}">
+          <h2><a href="${project.site}">${project.name}</a></h2>
           <p>${project.description}</p>
         </div>
       `
     projectContainer.appendChild(tempProject);
+    const projectElement = document.querySelector(`#${project.id}`)
+    projectElement.style.backgroundImage = `url("${project.image}")`
+    projectElement.addEventListener('mouseover', () => {
+      projectElement.style.backgroundImage = `url("${project.hoverImage}")`
+    })
+    projectElement.addEventListener('mouseout', () => {
+      projectElement.style.backgroundImage = `url("${project.image}")`
+    })
   }
 }
 
-//window.onload(main())
 document.addEventListener('DOMContentLoaded', main, false)
